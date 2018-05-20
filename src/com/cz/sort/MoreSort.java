57ad02b605a756ca3352296d18eb1d835c0e5cc8
chenzhao <chen95@mail.ustc.edu.cn>
2018-05-20 11:07:21 +0800

    add quick sort

--- src/com/cz/sort/MoreSort.java
+++ src/com/cz/sort/MoreSort.java
@@ -1,20 +1,67 @@
 package com.cz.sort;
 
 /**
- * ①快速排序 平均是O(nlogn)
- * ②归并排序 一直是O(nlogn)
+ * ①基数排序 平均是O(n)
+ * ②快速排序 平均是O(nlogn)
+ * ③归并排序 一直是O(nlogn)
  * @author ChenZhao
  *
  */
 public class MoreSort {
 
 	public static void main(String[] args) {
-		int[] a = {1,7,4,3,6,10,3,4};
-		int [] temp = new int[8];
-		MoreSort.mergeSort(a,temp, 0, a.length-1);
+		int[] a = {1,7,4,2,6,10,3,4};
+//		int [] temp = new int[8];
+		MoreSort.quickSort(a, 0, a.length-1);
 		for(int each: a) {
 			System.out.println(each);
 		}
+	}
+	public static void quickSort(int[] a, int first, int last) {
+		int MIN_SIZE = 4;//当小于4时，使用插入排序
+		if(last-first+1<MIN_SIZE) {
+			SortArray.insertSorted(a, first, last);
+		}else {
+			int pivotIndex = partition(a, first, last);
+			quickSort(a, first, pivotIndex - 1);
+			quickSort(a, pivotIndex + 1, last);
+		}
+	}
+	public static int partition(int[] a, int first, int last) {
+		int mid = first + (last-first) / 2;
+		//三元中值枢轴选择法，即在首、尾、中间值选择中位值
+		sortFirstMiddleLast(a, first, mid, last);
+		//交换枢轴到倒数第二个位置， 因为最后一个数肯定不小于枢轴，所有不用调整位置
+		swap(a, mid, last-1);
+		int pivotIndex = last - 1;
+		int pivotValue = a[pivotIndex];
+		int indexFromLeft = first + 1;
+		int indexFromRight = pivotIndex - 1;//last-2
+		boolean done = false;
+		while(!done) {
+			while(a[indexFromLeft]<pivotValue)
+				indexFromLeft++;
+			while(a[indexFromRight]>pivotValue)
+				indexFromRight--;
+			if(indexFromLeft<indexFromRight) {
+				swap(a, indexFromLeft, indexFromRight);
+				indexFromLeft++;
+				indexFromRight--;
+			}else {
+				done = true;
+			}
+		}
+		swap(a, pivotIndex, indexFromLeft);
+		pivotIndex = indexFromLeft;
+		return pivotIndex;
+	}
+	private static void sortFirstMiddleLast(int[] a, int first, int mid, int last) {
+		if(a[first]>a[last])
+			swap(a, first, last);
+		if(a[mid]>a[last])
+			swap(a, mid, last);
+		if(a[first]>a[mid])
+			swap(a, first, mid);
 	}
 	public static void mergeSort(int[] a, int[] tempArray, int first, int last) {
 		if(first<last) {
@@ -58,5 +105,10 @@ public class MoreSort {
 		//将临时数组拷贝到原数组
 		for(int i=first;i<index;i++)
 			a[i] = tempArray[i];
+	}
+	private static void swap(int[] a, int i, int j) {
+		int temp = a[j];
+		a[j] = a[i];
+		a[i] = temp;
 	}
 }
