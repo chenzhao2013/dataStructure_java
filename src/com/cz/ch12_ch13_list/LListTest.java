d63dbaa788f24f42e3f7612099da4cf0b9a67b54
chenzhao <chen95@mail.ustc.edu.cn>
2018-07-09 10:36:19 +0800

    modify test

--- src/com/cz/ch12_ch13_list/LListTest.java
+++ src/com/cz/ch12_ch13_list/LListTest.java
@@ -2,9 +2,13 @@ package com.cz.ch12_ch13_list;
 
 
 import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javafx.collections.transformation.SortedList;
 
 class LListTest {
 
@@ -14,28 +18,35 @@ class LListTest {
 
 	@Test
 	void testAdd() {
-		LList<String> list = new LList<>();
-		int index =0;
-		long timeStamp = System.currentTimeMillis();
-		System.out.println(timeStamp);
-		while(index!=100000) {
-			list.add(""+index);
-			index++;
-		}
-		System.out.println((System.currentTimeMillis()-timeStamp));
+//		LList<String> list = new LList<>();
+//		int index =0;
+//		long timeStamp = System.currentTimeMillis();
+//		System.out.println(timeStamp);
+//		while(index!=100000) {
+//			list.add(""+index);
+//			index++;
+//		}
+//		System.out.println((System.currentTimeMillis()-timeStamp));
 //		list.add("aa");
 //		list.add("bb");
 //		list.add("cc");
 //		list.add(2,"bb2");
-		long timeStamp2 = System.currentTimeMillis();
-		for(String item : list) {
+//		long timeStamp2 = System.currentTimeMillis();
+//		for(String item : list) {
+//			System.out.println(item);
+//		}
+//		System.out.println((System.currentTimeMillis()-timeStamp2));
+//		list.iterator();
+//		ArrayList<String> arrayList = new ArrayList<>();
+//		arrayList.listIterator();
+//		arrayList.iterator();
+		TreeSet<String> treeSet = new TreeSet<>();
+		treeSet.add("b");
+		treeSet.add("a");
+		treeSet.add("c");
+		for(String item : treeSet) {
 			System.out.println(item);
 		}
-		System.out.println((System.currentTimeMillis()-timeStamp2));
-//		list.iterator();
-		ArrayList<String> arrayList = new ArrayList<>();
-		arrayList.listIterator();
-		arrayList.iterator();
 	}
 
 }
