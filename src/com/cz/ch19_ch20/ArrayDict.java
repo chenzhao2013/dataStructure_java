5cb8e83774c43e09cd6493d9c29367f551331ad7
chenzhao <chen95@mail.ustc.edu.cn>
2018-07-23 12:41:58 +0800

    finish dictionary

--- src/com/cz/ch19_ch20/ArrayDict.java
+++ src/com/cz/ch19_ch20/ArrayDict.java
@@ -1,8 +1,10 @@
 package com.cz.ch19_ch20;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
 import com.sun.glass.ui.Size;
 
 public class ArrayDict<K, V> implements DictionaryInterface<K, V> {
@@ -22,6 +24,19 @@ public class ArrayDict<K, V> implements DictionaryInterface<K, V> {
 		initilized = true;
 	}
 
+	public ArrayDict() {
+		this(DEFAULT_CAPACITY);
+	}
+
+	private void checkInitilized() {
+		if(!initilized) {
+			throw new IllegalStateException();
+		}
+	}
+	/**
+	 * 数组长度检测
+	 * @param capacity
+	 */
 	private void checkCapacity(int capacity) {
 		if(capacity>=MAX_CAPACITY) {
 			throw new IllegalStateException("Attempt to create a bag whose "
@@ -57,56 +72,105 @@ public class ArrayDict<K, V> implements DictionaryInterface<K, V> {
 	}
 	@Override
 	public V add(K key, V value) {
-		// TODO Auto-generated method stub
-		return null;
+		checkInitilized();
+		if(key==null || value==null) {
+			throw new IllegalArgumentException();
+		}else {
+			V result = null;
+			int index = locateIndex(key);
+			if(index > 0 && index < numOfEntries) {
+				result = dictionary[index].getValue();
+				dictionary[index].setValue(value);
+			} else {
+				dictionary[numOfEntries] = new Entry<K, V>(key, value);
+				numOfEntries++;
+				ensureCapacity();
+			}
+			return result;
+		}
+		
 	}
 
 	@Override
 	public V remove(K key) {
-		// TODO Auto-generated method stub
-		return null;
+		checkInitilized();
+		V result = null;
+		int index = locateIndex(key);
+		if(index < numOfEntries) {
+			result = dictionary[index].getValue();
+			dictionary[index] = dictionary[numOfEntries-1];
+			dictionary[numOfEntries-1] = null;
+			numOfEntries--;
+		}
+		return result;
 	}
 
 	@Override
 	public V getValue(K key) {
-		// TODO Auto-generated method stub
-		return null;
+		V result = null;
+		int index = locateIndex(key);
+		if(index < numOfEntries) {
+			result = dictionary[index].getValue();
+		}
+		return result;
 	}
 
 	@Override
 	public boolean contains(K key) {
-		// TODO Auto-generated method stub
-		return false;
+		return getValue(key)!=null;
 	}
 
 	@Override
 	public Iterator<K> getKeyIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Iterator<K>() {
+
+			int index = 0;
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return index < numOfEntries;
+			}
+
+			@Override
+			public K next() {
+				// TODO Auto-generated method stub
+				return dictionary[index++].getKey();
+			}
+		};
 	}
 
 	@Override
 	public Iterator<V> getValueIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Iterator<V>() {
+			int index = 0;
+			@Override
+			public boolean hasNext() {
+				return index < numOfEntries;
+			}
+
+			@Override
+			public V next() {
+				return dictionary[index++].getValue();
+			}
+		};
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return numOfEntries == 0;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numOfEntries;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		for(int i=0; i<numOfEntries; i++) {
+			dictionary[i] = null;
+		}
+		HashMap<String, Integer> hashMap = new HashMap<>();
 	}
 
 }
